2a17c05b82d06408a1356ea3559dac8f2b81e377
Alen Sony <202894429+AlenSony@users.noreply.github.com>
2026-08-16 22:14:43 +0530

    Time: 68 ms (28.57%), Space: 20.2 MB (85.71%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.py
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -6,19 +6,12 @@ class Solution(object):
         """
 
         cnt = [0, 0, 0]
+
         for x in stones:
             cnt[x % 3] += 1
-            
-        c0, c1, c2 = cnt[0], cnt[1], cnt[2]
-        
-        # If no 1s or 2s are available, Alice cannot even make a valid first move
-        if c1 == 0 and c2 == 0:
-            return False
 
-        # When c0 is even, c0 does not change turn parity
-        if c0 % 2 == 0:
-            return c1 > 0 and c2 > 0
+        if cnt[0] % 2 == 0:
+            return cnt[1] > 0 and cnt[2] > 0
         else:
-            # When c0 is odd, it flips the winning advantage
-            return abs(c1 - c2) > 2
+            return abs(cnt[1] - cnt[2]) > 2
         
